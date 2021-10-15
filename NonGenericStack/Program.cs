68d77ab8fd49843adcb44ce685b940af501de4ae
vishal.Alok <you@example.com>
2021-10-15 06:13:26 +0530

    Add project files.

---
+++ NonGenericStack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace NonGenericStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack obj = new Stack();
+            obj.Push("Hello");
+            obj.Push(10);
+            obj.Push(true);
+            //Printing all in LIFO
+            foreach (object o in obj)
+            {
+                Console.WriteLine(o + " ");
+            }
+            //Printing top
+            Console.WriteLine(obj.Peek());
+            //Printing top and removing
+            Console.WriteLine(obj.Pop());
+        }
+    }
+}
